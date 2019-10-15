e76e10d7093f845b9592673aa46224f7dfbc3d92
cjogles <jackson.ogles28@gmail.com>
2019-10-14 18:10:43 -0600

    added axios and created a place for get requests

--- README.old.md
+++ README.old.md
@@ -1,2 +1,8 @@
 # Your a Wizard Harry
 Enter Hogwarts School of witch-craft and wizardry and join a Hogwarts House. 
+
+I bootstrapped this application using React. I implemented react router to perform some client side routing to increase performance with the recognition that this will inhibit some possible benefits with SEO (search engine optimization) which with the scale of this project I don't care much about anyways. This is primarily a portfolio piece that I want to build to showcase some skills I have developed. 
+
+I installed axios as a powerful HTTP request provider to peform GET requests on the Harry Potter API in my react app. This library supports the Promise API. 
+
+
--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, Link} from 'react-router-dom';
+import axios from 'axios';
 
 function App() {
+
+axios
+  .get('https://www.potterapi.com/v1/sortingHat')
+    .then(function (response) {
+    console.log(response);
+    })
+    .catch(function (error) {
+    console.log(`There was an error. The error is: ${error}`);
+    });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Your a Wizard Harry.</h1>
     </div>
   );
 }
